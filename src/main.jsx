4f414282c68b4604483b2a0ef2452463e18ef2aa
victorgsa1 <vgsa@hotmail.com>
2023-09-05 17:26:16 -0300

    Chakra UI First Steps

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import theme from './styles/theme.ts'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+)
