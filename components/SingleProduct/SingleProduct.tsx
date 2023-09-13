b4b07ebafc57542833f1ae29ecd7b9cb5da19fbb
victor <vgsa@hotmail.com>
2023-09-13 15:03:28 -0300

    Query Single Product

--- components/SingleProduct/SingleProduct.tsx
+++ components/SingleProduct/SingleProduct.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
-import { HStack, Flex, Button,  } from '@chakra-ui/react'
+import React from 'react';
+import { Box, Image, Heading, Text, Button } from '@chakra-ui/react';
 
-export default function SingleProduct () {
+function SingleProduct({ product }) {
   return (
-    <HStack w="full" justify="center" bg='#f0f0f5' pt='8'>
-      
-    </HStack>
-)}
+    <Box maxW="md" borderWidth="1px" borderRadius="lg" overflow="hidden">
+      <Image src={product.image_url} alt={product.name} />
+
+      <Box p="4">
+        <Heading size="md" fontFamily="brand" fontWeight="light" pb="2">
+          {product.name}
+        </Heading>
+        <hr />
+        <Text fontSize="md" fontWeight="bold" color="black" pt="2">
+          R$ {(product.price_in_cents / 100).toLocaleString('pt-BR', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          })}
+        </Text>
+        <Text fontSize="sm" color="gray.500">
+          Categoria: {product.category}
+        </Text>
+        <Text fontSize="sm" color="gray.500" mt="2">
+          Descrição: {product.description}
+        </Text>
+        <Button mt="4" colorScheme="black" size="sm" variant="outline">
+          Adicionar ao Carrinho
+        </Button>
+      </Box>
+    </Box>
+  );
+}
+
+export default SingleProduct;
--- pages/product/[productId].js
+++ pages/product/[productId].js
@@ -1,16 +1,39 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import Header from '@/components/Header/Header';
-import Test from '../test';
-import { Image } from '@chakra-ui/react';
+import SingleProduct from '../../components/SingleProduct/SingleProduct';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_PRODUCT_BY_ID = gql`
+  query GetProductById($productId: ID!) {
+    product(id: $productId) {
+      id
+      name
+      description
+      image_url
+      price_in_cents
+      category
+    }
+  }
+`;
 
 function ProductDetails() {
   const router = useRouter();
-  const { productId } = router.query; 
+  const { productId } = router.query;
+
+  const { loading, error, data } = useQuery(GET_PRODUCT_BY_ID, {
+    variables: { productId },
+  });
+
+  if (loading) return <p>Carregando...</p>;
+  if (error) return <p>Erro: {error.message}</p>;
+
+  const product = data.product;
 
   return (
     <div>
-      <Header/>
+      <Header />
+      <SingleProduct product={product} />
     </div>
   );
 }
